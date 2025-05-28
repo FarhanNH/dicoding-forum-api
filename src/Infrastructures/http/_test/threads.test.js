d3d9686370c236c09b6c5542f24542549a1ff95b
FarhanNH <farhan.nhuda@gmail.com>
2025-05-28 09:01:15 +0700

    add threads test case

---
+++ src/Infrastructures/http/_test/threads.test.js
@@ -0,0 +1,70 @@
+const pool = require('../../database/postgres/pool');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const container = require('../../container');
+const createServer = require('../createServer');
+
+describe('/threads endpoint', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  describe('when POST /threads', () => {
+    it('should response 201 and persisted thread', async () => {
+      // Arrange
+      const server = await createServer(container);
+      const mockUser = {
+        username: 'dicoding',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      };
+
+      const userResponse = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: mockUser,
+      });
+
+      const userResponseJson = JSON.parse(userResponse.payload);
+      const user = userResponseJson.data.addedUser;
+
+      const loginResponse = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: {
+          username: user.username,
+          password: mockUser.password,
+        },
+      });
+
+      const loginResponseJson = JSON.parse(loginResponse.payload);
+      const accessToken = loginResponseJson.data.accessToken;
+
+      const requestPayload = {
+        title: 'First Thread',
+        body: 'Lorem ipsum asdadadjakkafkahfkakfdajkfj',
+      };
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedThread).toBeDefined();
+    });
+  });
+});
